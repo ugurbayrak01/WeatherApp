b0747a17e3fbd56dcf8559ee6d47a42c47532838
Uğur Bayrak <ugurbayrak001@gmail.com>
2024-02-08 14:17:11 +0300

    Initial commit

---
+++ app/src/main/java/com/ugurbayrak/weatherapp/presentation/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.ugurbayrak.weatherapp.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.ugurbayrak.weatherapp.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
