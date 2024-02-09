240c13faae850d17ce123a941f36c0c13efb1636
Uğur Bayrak <56801405+ugurbayrak01@users.noreply.github.com>
2024-02-09 23:52:46 +0300

    Update Constants.kt

--- app/src/main/java/com/ugurbayrak/weatherapp/util/Constants.kt
+++ app/src/main/java/com/ugurbayrak/weatherapp/util/Constants.kt
@@ -2,6 +2,6 @@ package com.ugurbayrak.weatherapp.util
 
 object Constants {
     const val BASE_URL = "https://api.openweathermap.org"
-    const val API_KEY = "b3df5c6dbe8deb416d78854d5b50758f"
+    const val API_KEY = "API_KEY"
     const val UNITS = "metric"
 }
