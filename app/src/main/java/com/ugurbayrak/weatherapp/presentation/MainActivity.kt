a6910e1556c15dbd4172e5dc6c6130e41e52711e
Uğur Bayrak <ugurbayrak001@gmail.com>
2024-02-17 11:44:11 +0300

    Add Google Maps and Toolbar

--- app/src/main/java/com/ugurbayrak/weatherapp/presentation/MainActivity.kt
+++ app/src/main/java/com/ugurbayrak/weatherapp/presentation/MainActivity.kt
@@ -2,13 +2,54 @@ package com.ugurbayrak.weatherapp.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
+import com.google.android.material.appbar.MaterialToolbar
 import com.ugurbayrak.weatherapp.R
+import com.ugurbayrak.weatherapp.presentation.weather.view.WeatherFragmentDirections
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var navigationController: NavController
+    private lateinit var toolbar: MaterialToolbar
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        navigationController = navHostFragment.navController
+        NavigationUI.setupActionBarWithNavController(this,navigationController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return NavigationUI.navigateUp(navigationController,null)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater: MenuInflater = menuInflater
+        inflater.inflate(R.menu.menu_toolbar, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.menu_item -> {
+                val action = WeatherFragmentDirections.actionWeatherFragmentToMapsFragment()
+                findNavController(R.id.fragmentContainerView).navigate(action)
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 }
